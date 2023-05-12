700ccf6ca3f28a5631b92c4cfd26b898806794ea
Juan Perona <insoundj@hotmail.com>
2023-05-12 20:08:33 +0200

    lexik_jwt instalado y configurado

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/registro", name="api_registro")
+     */
+    public function registro(UserPasswordHasherInterface $passHasher, UserRepository $userRepository, Request $request)
+    {
+        $user = new User();
+        $user->setEmail('juan@gmail.com');
+        $passTextPlain = "123456";
+        $passHashed = $passHasher->hashPassword($user, $passTextPlain);
+        $user->setPassword($passHashed);
+        $userRepository->add($user,true);
+
+        return $this->json(["respuesta"=>$user]);
+    }
+
+    /**
+     * @Route("/api/login", name="api_login")
+     */
+    public function login()
+    {
+        return $this->json(["respuesta"=>"login"]);
+    }    
+}
